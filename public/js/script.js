2d9fd2eade14ec296bd77780f7f07e8cbb31c93d
xded <xded@mail.ru>
2020-01-20 15:00:00 +0300

    show modal

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,29 @@
-console.log(3 + '2');
-let a = 5;
-a = 8 + a; //13 -> a
-console.log(a);
-//alert(a);
-//out.innerHTML = '<i>Hi</i>';
-//out.innerText = '<i>Hi</i>';
-//document.getElementById('out').innerHTML = 777;
-let p;
-p = document.getElementById('out');
-p = document.querySelector('#out');
-p.innerHTML = 558885;
-console.log(p);
-let c = 'hello';
-let d = 'world';
-console.log(c + d);
-// let year = 
+function hello() {
+    console.log('Hello');
+    hello2();
+}
+
+function hello2() {
+    console.log('hello-2-2-2-22');
+}
+
+
+let btn = document.querySelector('#btn'); // кнопка
+console.log(btn.id);
+btn.onclick = hello;
+// hello();
+//btn.hidden = true;
+
+
+let showModal = document.querySelector('#modal-show'); // кнопку show modal
+
+function show() {
+    let modal = document.querySelector('.modal');
+    //modal.hidden = true;
+    modal.style.display = 'block';
+    modal.style.backgroundColor = 'pink';
+}
+
+showModal.onclick = show;
+//border-radius: 20px;
+//border  - 5px;
