df7f4e43336208b30ecce0ee325c5d9fcdbc4343
xded <xded@mail.ru>
2020-01-20 14:56:49 +0300

    querySelector

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,17 @@
-console.log(2 - 1);
-alert('hello');
-alert("hello");
-console.log('fffffff');
+console.log(3 + '2');
+let a = 5;
+a = 8 + a; //13 -> a
+console.log(a);
+//alert(a);
+//out.innerHTML = '<i>Hi</i>';
+//out.innerText = '<i>Hi</i>';
+//document.getElementById('out').innerHTML = 777;
+let p;
+p = document.getElementById('out');
+p = document.querySelector('#out');
+p.innerHTML = 558885;
+console.log(p);
+let c = 'hello';
+let d = 'world';
+console.log(c + d);
+// let year = 
