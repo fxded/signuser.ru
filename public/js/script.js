908323f1bac4cd79749e39806db57eefa14f5e31
xded <xded@mail.ru>
2020-01-20 15:20:42 +0300

    input data

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,33 @@
-// парный тег
-document.querySelector('p').innerHTML += ' Пример span <b>Hi</b>';
-document.querySelector('p').innerText += ' Пример span <b>Hi</b>';
-// -----------------------
-
-document.querySelector('h1').outerHTML = '<h2>jjjj</h2>';
-document.querySelector('h2').outerText = '<h2>jjjj</h2>';
-// ----------------------
-
-document.querySelector('div').insertAdjacentHTML('beforebegin', 2222);
-document.querySelector('div').insertAdjacentHTML('afterbegin', 333);
-document.querySelector('div').insertAdjacentHTML('afterend', 5555);
-document.querySelector('div').insertAdjacentHTML('beforeend', 4444);
-
-//-----------------------
-
-document.querySelector('.one span').innerHTML = 9999;
-// ----------------------
-console.log(document.querySelector('img').alt);
-document.querySelector('img').alt = 'yellow cat';
+document.querySelector('.push').onclick = function () {
+    // данные из input type = text
+    console.log(document.querySelector('.text-input').value);
 
 
-console.log(document.querySelector('img').src);
-document.querySelector('img').src = 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/star-48.png';
+    // данные из input type = password
+    console.log(document.querySelector('.pass-input').value);
 
-document.querySelector('title').innerText = 'Lesson 4';
 
-console.log(document.querySelector('link').href);
-document.querySelector('link').href = 'css/style2.css';
+    // данные из input type = range
+    console.log(document.querySelector('.range-input').value);
 
-// -------------------------
-console.log(document.querySelector('input').value);
-document.querySelector('input').value = '13.12.2018';
-document.querySelector('input').type = 'range';
+    // данные из input type = date
+    console.log(document.querySelector('.calendar-input').value);
+
+    // данные из input type = color
+    console.log(document.querySelector('.color-input').value);
+
+    // данные из input type = checkbox
+    console.log(document.querySelector('#checkbox-input').checked);
+    if (document.querySelector('#checkbox-input').checked) {
+        console.log('Даааааа!');
+    }
+    else {
+        console.log('Heeeeeт');
+    }
+
+    console.log('----------------Radio------------');
+
+    // данные из input type = radio
+    // console.log(document.querySelector('.radio-input[checked="checked"]').value);
+
+}
