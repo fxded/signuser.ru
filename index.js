83d0f21640118fe81ac3bef179c552719cfec2a2
xded <xded@mail.ru>
2020-01-22 19:15:29 +0300

    return post string

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ var port = process.env.PORT || 3002,
     fs = require('fs');
 
 
-var app = express(),mp
-    io = socket.listen(app.listen(port));
-
+var app = express();
+//    io = socket.listen(app.listen(port));
+app.listen(port);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
     res.sendfile('index.html');
 });
 
+app.post ('/', function(req,res){
+    req.on('data', function(data){
+        console.log('requset: ', data.toString());   
+        res.write('Write: '+ data.toString());
+    });
+    req.on('end', function(){
+        console.log('the end ');
+        res.end();
+    });
+});
+
+
+console.log('Listen on ' + port + ' dir ' + __dirname);
